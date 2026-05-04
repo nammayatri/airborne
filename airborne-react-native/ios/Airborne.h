156bdb1b64d3cd86c2b365bda639c91512196a04
PraveenGongada <praveengongada0603@gmail.com>
2026-05-04 15:46:33 +0530

    fix: react native linking error

--- airborne-react-native/ios/Airborne.h
+++ airborne-react-native/ios/Airborne.h
@@ -3,7 +3,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface Airborne : NSObject
+@interface AirborneInstance : NSObject
 
 + (instancetype)sharedInstanceWithNamespace:(NSString *)aNamespace;
 
--- airborne-react-native/ios/AirborneReact.mm
+++ airborne-react-native/ios/AirborneReact.mm
@@ -27,114 +27,136 @@ static NSString * const defaultNamespace = @"default";
 }
 
 #ifdef RCT_NEW_ARCH_ENABLED
-- (void)readReleaseConfig:(RCTPromiseResolveBlock)resolve
+- (void)readReleaseConfig:(NSString *)nameSpace
+                  resolve:(RCTPromiseResolveBlock)resolve
                    reject:(RCTPromiseRejectBlock)reject {
     @try {
-        NSString *config = [[Airborne sharedInstanceWithNamespace:defaultNamespace] getReleaseConfig];
+        NSString *ns = (nameSpace.length > 0) ? nameSpace : defaultNamespace;
+        NSString *config = [[AirborneInstance sharedInstanceWithNamespace:ns] getReleaseConfig];
         resolve(config);
     } @catch (NSException *exception) {
         reject(@"AIRBORNE_ERROR", exception.reason, nil);
     }
 }
 
-- (void)getFileContent:(NSString *)filePath
+- (void)getFileContent:(NSString *)nameSpace
+              filePath:(NSString *)filePath
                resolve:(RCTPromiseResolveBlock)resolve
                 reject:(RCTPromiseRejectBlock)reject {
     @try {
-        NSString *content = [[Airborne sharedInstanceWithNamespace:defaultNamespace] getFileContent:filePath];
+        NSString *ns = (nameSpace.length > 0) ? nameSpace : defaultNamespace;
+        NSString *content = [[AirborneInstance sharedInstanceWithNamespace:ns] getFileContent:filePath];
         resolve(content);
     } @catch (NSException *exception) {
         reject(@"AIRBORNE_ERROR", exception.reason, nil);
     }
 }
 
-- (void)getBundlePath:(RCTPromiseResolveBlock)resolve
+- (void)getBundlePath:(NSString *)nameSpace
+              resolve:(RCTPromiseResolveBlock)resolve
                reject:(RCTPromiseRejectBlock)reject {
     @try {
-        NSString *bundlePath = [[Airborne sharedInstanceWithNamespace:defaultNamespace] getBundlePath];
+        NSString *ns = (nameSpace.length > 0) ? nameSpace : defaultNamespace;
+        NSString *bundlePath = [[AirborneInstance sharedInstanceWithNamespace:ns] getBundlePath];
         resolve(bundlePath);
     } @catch (NSException *exception) {
         reject(@"AIRBORNE_ERROR", exception.reason, nil);
     }
 }
 
-- (void)checkForUpdate:(RCTPromiseResolveBlock)resolve
+- (void)checkForUpdate:(NSString *)nameSpace
+               resolve:(RCTPromiseResolveBlock)resolve
                 reject:(RCTPromiseRejectBlock)reject {
     reject(@"AIRBORNE_NOT_IMPLEMENTED_IOS", @"checkForUpdate is not implemented on iOS", nil);
 }
 
-- (void)downloadUpdate:(RCTPromiseResolveBlock)resolve
+- (void)downloadUpdate:(NSString *)nameSpace
+               resolve:(RCTPromiseResolveBlock)resolve
                 reject:(RCTPromiseRejectBlock)reject {
     reject(@"AIRBORNE_NOT_IMPLEMENTED_IOS", @"downloadUpdate is not implemented on iOS", nil);
 }
 
-- (void)startBackgroundDownload:(RCTPromiseResolveBlock)resolve
+- (void)startBackgroundDownload:(NSString *)nameSpace
+                        resolve:(RCTPromiseResolveBlock)resolve
                          reject:(RCTPromiseRejectBlock)reject {
     reject(@"AIRBORNE_NOT_IMPLEMENTED_IOS", @"startBackgroundDownload is not implemented on iOS", nil);
 }
 
-- (void)reloadApp:(RCTPromiseResolveBlock)resolve
+- (void)reloadApp:(NSString *)nameSpace
+          resolve:(RCTPromiseResolveBlock)resolve
            reject:(RCTPromiseRejectBlock)reject {
     reject(@"AIRBORNE_NOT_IMPLEMENTED_IOS", @"reloadApp is not implemented on iOS", nil);
 }
 
-- (void)hasPendingBundleUpdate:(RCTPromiseResolveBlock)resolve
+- (void)hasPendingBundleUpdate:(NSString *)nameSpace
+                       resolve:(RCTPromiseResolveBlock)resolve
                         reject:(RCTPromiseRejectBlock)reject {
     reject(@"AIRBORNE_NOT_IMPLEMENTED_IOS", @"hasPendingBundleUpdate is not implemented on iOS", nil);
 }
 #else
-RCT_EXPORT_METHOD(readReleaseConfig:(RCTPromiseResolveBlock)resolve
+RCT_EXPORT_METHOD(readReleaseConfig:(NSString *)nameSpace
+                  resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     @try {
-        NSString *config = [[Airborne sharedInstanceWithNamespace:defaultNamespace] getReleaseConfig];
+        NSString *ns = (nameSpace.length > 0) ? nameSpace : defaultNamespace;
+        NSString *config = [[AirborneInstance sharedInstanceWithNamespace:ns] getReleaseConfig];
         resolve(config);
     } @catch (NSException *exception) {
         reject(@"AIRBORNE_ERROR", exception.reason, nil);
     }
 }
 
-RCT_EXPORT_METHOD(getFileContent:(NSString *)filePath
+RCT_EXPORT_METHOD(getFileContent:(NSString *)nameSpace
+                  filePath:(NSString *)filePath
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     @try {
-        NSString *content = [[Airborne sharedInstanceWithNamespace:defaultNamespace] getFileContent:filePath];
+        NSString *ns = (nameSpace.length > 0) ? nameSpace : defaultNamespace;
+        NSString *content = [[AirborneInstance sharedInstanceWithNamespace:ns] getFileContent:filePath];
         resolve(content);
     } @catch (NSException *exception) {
         reject(@"AIRBORNE_ERROR", exception.reason, nil);
     }
 }
 
-RCT_EXPORT_METHOD(getBundlePath:(RCTPromiseResolveBlock)resolve
+RCT_EXPORT_METHOD(getBundlePath:(NSString *)nameSpace
+                  resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     @try {
-        NSString *bundlePath = [[Airborne sharedInstanceWithNamespace:defaultNamespace] getBundlePath];
+        NSString *ns = (nameSpace.length > 0) ? nameSpace : defaultNamespace;
+        NSString *bundlePath = [[AirborneInstance sharedInstanceWithNamespace:ns] getBundlePath];
         resolve(bundlePath);
     } @catch (NSException *exception) {
         reject(@"AIRBORNE_ERROR", exception.reason, nil);
     }
 }
 
-RCT_EXPORT_METHOD(checkForUpdate:(RCTPromiseResolveBlock)resolve
+RCT_EXPORT_METHOD(checkForUpdate:(NSString *)nameSpace
+                  resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     reject(@"AIRBORNE_NOT_IMPLEMENTED_IOS", @"checkForUpdate is not implemented on iOS", nil);
 }
 
-RCT_EXPORT_METHOD(downloadUpdate:(RCTPromiseResolveBlock)resolve
+RCT_EXPORT_METHOD(downloadUpdate:(NSString *)nameSpace
+                  resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     reject(@"AIRBORNE_NOT_IMPLEMENTED_IOS", @"downloadUpdate is not implemented on iOS", nil);
 }
 
-RCT_EXPORT_METHOD(startBackgroundDownload:(RCTPromiseResolveBlock)resolve
+RCT_EXPORT_METHOD(startBackgroundDownload:(NSString *)nameSpace
+                  resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     reject(@"AIRBORNE_NOT_IMPLEMENTED_IOS", @"startBackgroundDownload is not implemented on iOS", nil);
 }
 
-RCT_EXPORT_METHOD(reloadApp:(RCTPromiseResolveBlock)resolve
+RCT_EXPORT_METHOD(reloadApp:(NSString *)nameSpace
+                  resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     reject(@"AIRBORNE_NOT_IMPLEMENTED_IOS", @"reloadApp is not implemented on iOS", nil);
 }
 
-RCT_EXPORT_METHOD(hasPendingBundleUpdate:(RCTPromiseResolveBlock)resolve
+RCT_EXPORT_METHOD(hasPendingBundleUpdate:(NSString *)nameSpace
+                  resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     reject(@"AIRBORNE_NOT_IMPLEMENTED_IOS", @"hasPendingBundleUpdate is not implemented on iOS", nil);
 }
